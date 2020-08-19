33b4c648c29c4f3e53398ec1932add8dd840bb2b
Adam Olech <nddr89@gmail.com>
2020-08-19 20:44:07 +0200

    obj2opengl-fixup: glob support

--- obj2opengl-fixup.py
+++ obj2opengl-fixup.py
@@ -2,14 +2,16 @@
 
 import sys, os
 
-def find_start_end():
-    with open(sys.argv[1], "r") as fh:
+def find_start_end(file_path):
+    with open(file_path, "r") as fh:
         lines = fh.readlines()
+
+    file_name = os.path.basename(file_path)
 
     arr_end = "};\n"
 
-    vert_line = "float {}Verts [] = {{\n".format(sys.argv[1].split(".")[0])
-    norm_line = "float {}Normals [] = {{\n".format(sys.argv[1].split(".")[0])
+    vert_line = "float {}Verts [] = {{\n".format(file_name.split(".")[0])
+    norm_line = "float {}Normals [] = {{\n".format(file_name.split(".")[0])
 
     vert_start = lines.index(vert_line)+2
     norm_start = lines.index(norm_line)+2
@@ -25,18 +27,23 @@ def find_start_end():
 
     return data
 
-def call_sed(lr):
+def call_sed(file_path, lr):
     cmd = [
-            "sed -i '{},{}s/$/1.0f,/' {}".format(lr['vert'][0],lr['vert'][1],sys.argv[1]),
-            "sed -i '{},{}s/$/0.0f,/' {}".format(lr['norm'][0],lr['norm'][1],sys.argv[1]),
+            "sed -i '{},{}s/$/1.0f,/' {}".format(lr['vert'][0],lr['vert'][1],file_path),
+            "sed -i '{},{}s/$/0.0f,/' {}".format(lr['norm'][0],lr['norm'][1],file_path),
     ]
 
     for c in cmd:
         os.system(c)
 
 def main():
-    line_ranges = find_start_end()
-    print(line_ranges)
-    call_sed(line_ranges)
+    for f in sys.argv[1:]:
+        fs = f.split(".")
+        if len(fs) != 2 or fs[1] != "h":
+            print("Rejecting {}".format(f))
+            continue
+        line_ranges = find_start_end(f)
+        print(f, line_ranges)
+        call_sed(f, line_ranges)
 
 main()
