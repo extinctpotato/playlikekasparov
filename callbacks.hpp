4deb7c70bbf54666d7b3f0a5864f37c9f14c4168
Adam Olech <nddr89@gmail.com>
2020-08-27 22:19:09 +0200

    Scrap callbacks for now, we don't need that kinda separation

--- callbacks.hpp
+++
@@ -1,3 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-void windowResizeCallback(GLFWwindow *window,int width,int height);
-void error_callback(int error,const char *description);
