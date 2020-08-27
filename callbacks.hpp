629821d34be9a28ce56dfa1ec35e2a24124e430b
Adam Olech <nddr89@gmail.com>
2020-08-27 21:32:04 +0200

    Copy 2 callbacks

---
+++ callbacks.hpp
@@ -0,0 +1,3 @@
+/* This file was automatically generated.  Do not edit! */
+void windowResizeCallback(GLFWwindow *window,int width,int height);
+void error_callback(int error,const char *description);
