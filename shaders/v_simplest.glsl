c9b9d07852db85868028ad511216f7eb70728383
Adam Olech <nddr89@gmail.com>
2020-09-06 14:00:24 +0200

    Simplest shader: add layout

--- shaders/v_simplest.glsl
+++ shaders/v_simplest.glsl
@@ -6,9 +6,9 @@ uniform mat4 V;
 uniform mat4 M;
 
 //Atrybuty
-in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec4 normal; //wektor normalny w przestrzeni modelu
-in vec2 texCoord0;
+layout (location = 0) in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
+layout (location = 1) in vec4 normal; //wektor normalny w przestrzeni modelu
+layout (location = 2) in vec2 texCoord0;
 
 //Zmienne interpolowane
 out vec4 l;
