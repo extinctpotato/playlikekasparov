51013edb71e061e7cb1bce57439730d58a5c532a
Adam Olech <nddr89@gmail.com>
2020-09-06 13:36:09 +0200

    Remove some stuff from shaderprogram.h

--- shaderprogram.h
+++ shaderprogram.h
@@ -1,31 +1,8 @@
-/*
-Niniejszy program jest wolnym oprogramowaniem; możesz go
-rozprowadzać dalej i / lub modyfikować na warunkach Powszechnej
-Licencji Publicznej GNU, wydanej przez Fundację Wolnego
-Oprogramowania - według wersji 2 tej Licencji lub(według twojego
-wyboru) którejś z późniejszych wersji.
-
-Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on
-użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej
-gwarancji PRZYDATNOśCI HANDLOWEJ albo PRZYDATNOśCI DO OKREśLONYCH
-ZASTOSOWAń.W celu uzyskania bliższych informacji sięgnij do
-Powszechnej Licencji Publicznej GNU.
-
-Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
-Powszechnej Licencji Publicznej GNU(GNU General Public License);
-jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
-Place, Fifth Floor, Boston, MA  02110 - 1301  USA
-*/
-
-
 #ifndef SHADERPROGRAM_H
 #define SHADERPROGRAM_H
 
-
 #include <GL/glew.h>
 #include "stdio.h"
-
-
 
 class ShaderProgram {
 private:
@@ -42,8 +19,5 @@ public:
 	GLuint u(const char* variableName); //Pobiera numer slotu związanego z daną zmienną jednorodną
 	GLuint a(const char* variableName); //Pobiera numer slotu związanego z danym atrybutem
 };
-
-
-
 
 #endif
