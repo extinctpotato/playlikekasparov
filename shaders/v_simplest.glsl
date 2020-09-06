3419ac8315506bb99157172b7ca8542aaf8e28cf
Adam Olech <nddr89@gmail.com>
2020-09-06 13:53:51 +0200

    Simplest shader: remove color attrib

--- shaders/v_simplest.glsl
+++ shaders/v_simplest.glsl
@@ -7,12 +7,10 @@ uniform mat4 M;
 
 //Atrybuty
 in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec4 color; //kolor związany z wierzchołkiem
 in vec4 normal; //wektor normalny w przestrzeni modelu
 in vec2 texCoord0;
 
 //Zmienne interpolowane
-out vec4 ic;
 out vec4 l;
 out vec4 n;
 out vec4 v;
@@ -25,7 +23,6 @@ void main(void) {
     n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
     
     iTexCoord0 = texCoord0;
-    ic = color;
     
     gl_Position=P*V*M*vertex;
 }
