92da987fd7a04ffa7e50a6a04061197369add8ca
Adam Olech <nddr89@gmail.com>
2020-09-14 21:57:02 +0200

    Add chessboard field to models/all.h

--- models/all.h
+++ models/all.h
@@ -17,11 +17,12 @@ struct Guacamole {
 	unsigned int vao;
 };
 
-Guacamole figures[6] = {
+Guacamole figures[] = {
 	{rookVerts,sizeof(rookVerts),rookNormals,sizeof(rookNormals),rookTexCoords,sizeof(rookTexCoords),rookNumVerts,0}, 
 	{knightVerts,sizeof(knightVerts),knightNormals,sizeof(knightNormals),knightTexCoords,sizeof(knightTexCoords),knightNumVerts,0}, 
 	{bishopVerts,sizeof(bishopVerts),bishopNormals,sizeof(bishopNormals),bishopTexCoords,sizeof(bishopTexCoords),bishopNumVerts,0}, 
 	{queenVerts,sizeof(queenVerts),queenNormals,sizeof(queenNormals),queenTexCoords,sizeof(queenTexCoords),queenNumVerts,0}, 
 	{kingVerts,sizeof(kingVerts),kingNormals,sizeof(kingNormals),kingTexCoords,sizeof(kingTexCoords),kingNumVerts,0}, 
 	{pawnVerts,sizeof(pawnVerts),pawnNormals,sizeof(pawnNormals),pawnTexCoords,sizeof(pawnTexCoords),pawnNumVerts,0}, 
+	{fieldVerts,sizeof(fieldVerts),fieldNormals,sizeof(fieldNormals),fieldTexCoords,sizeof(fieldTexCoords),fieldNumVerts,0}, 
 };
