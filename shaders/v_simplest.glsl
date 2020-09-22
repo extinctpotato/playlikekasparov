3a0946f13e1dc716177ed31dab28c91748cf8e79
Adam Olech <nddr89@gmail.com>
2020-09-22 23:51:09 +0200

    Removed some comments

--- shaders/v_simplest.glsl
+++ shaders/v_simplest.glsl
@@ -1,16 +1,13 @@
 #version 330
 
-//Zmienne jednorodne
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-//Atrybuty
 layout (location = 0) in vec4 vertex; 
 layout (location = 1) in vec4 normal; 
 layout (location = 2) in vec2 texCoord0;
 
-//Zmienne interpolowane
 out vec4 l;
 out vec4 n;
 out vec4 v;
